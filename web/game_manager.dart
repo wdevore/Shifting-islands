14e59e01230bac76cb7a23a2c826b9116831e02c
William DeVore <william.quartz@gmail.com>
2014-10-02 21:32:29 -0400

    Initial commit

---
+++ web/game_manager.dart
@@ -0,0 +1,16 @@
+part of shifting_islands;
+
+class GameManager {
+  static final GameManager _instance = new GameManager._internal();
+
+  GameManager._internal();
+  
+  Resources _resources = new Resources();
+  
+  // ----------------------------------------------------------
+  // Instance
+  // ----------------------------------------------------------
+  static GameManager get instance => _instance;
+
+  Resources get resources => _resources;
+}
